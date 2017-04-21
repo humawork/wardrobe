ed82860238c02eee5061c4f0a001e57a87950ed7
Eigil Sagafos <eigil@sagafos.no>
2017-04-21 16:34:05 +0200

    Added support for coercion when using Hash#[]=

--- lib/atrs/coercions/hash.rb
+++ lib/atrs/coercions/hash.rb
@@ -15,9 +15,9 @@ module Atrs
       def coerce(v, atr)
         case v
         when Hash
-          coerce_hash(v)
+          coerce_hash(v, atr)
         when Array
-          coerce_hash(self.class[*v])
+          coerce_hash(self.class[*v], atr)
         when NilClass then {}
         else
           raise UnsupportedError
@@ -26,11 +26,33 @@ module Atrs
 
       private
 
-      def coerce_hash(h)
-        h.map do |key, value|
+      def coerce_hash(h, atr)
+        hash = h.map do |key, value|
           #TODO: Since we pass around the atr object, how should this work here? Just pass nil?
           [self.first[0].coerce(key, nil), self.first[1].coerce(value, nil)]
         end.to_h
+
+        hash.singleton_class.class_exec(first, atr) do |coercer, atr_object|
+          @_coercer = coercer
+          @_atr = atr_object
+
+          def _coercer
+            self.singleton_class.instance_variable_get(:@_coercer)
+          end
+
+          def self._atr; @_atr; end
+
+          def _coerce(key, value)
+            return _coercer[0].coerce(key, nil),
+                   _coercer[1].coerce(value, nil)
+          end
+
+          def []=(key, value)
+            super(*_coerce(key,value))
+          end
+
+        end
+        hash
       end
     end
   end
--- test/unit/coercions/hash_test.rb
+++ test/unit/coercions/hash_test.rb
@@ -1,32 +1,54 @@
 require 'test_helper'
 
 class HashCoercionTest < Minitest::Test
+  class Person
+    include Atrs
+    attribute :name, String
+  end
+
   class HashObject
     include Atrs
     attribute :hash,           Hash
     attribute :string_symbol,  Hash[String => Symbol]
     attribute :integer_float,  Hash[Integer => Float]
     attribute :time_set,       Hash[Time => Set]
+    attribute :person_int,     Hash[Person => Integer]
+    attribute :string_person,  Hash[String => Person]
     attribute :nil,            Hash
   end
 
-  def test_coercion
-    now = Time.now
-    later = now + 1
-    object = HashObject.new(
+  def setup
+    @now = Time.now
+    @later = @now + 1
+    @object = HashObject.new(
       hash: { any: 'thing'},
       string_symbol: { 1 => 'one', :two => 'two'},
       integer_float: { '1' => 1, 2.0 => '2.2'},
-      time_set: { now.to_i => [1,1], later.to_s => Set.new([2,2])}
+      time_set: { @now.to_i => [1,1], @later.to_s => Set.new([2,2])},
+      person_int: { { name: 'Test' } => '1' },
+      string_person: { 1 => { name: 'Test' } }
     )
-    assert_equal 'thing', object.hash[:any]
-    assert_equal :one, object.string_symbol['1']
-    assert_equal :two, object.string_symbol['two']
-    assert_equal 1.0, object.integer_float[1]
-    assert_equal 2.2, object.integer_float[2]
-    assert_equal Set.new([1]), object.time_set[Time.at(now.to_i)]
-    assert_equal Set.new([2]), object.time_set[Time.at(later.to_i)]
-    assert_equal Hash.new, object.nil
+  end
+
+  def test_coercion
+    assert_equal 'thing', @object.hash[:any]
+    assert_equal :one, @object.string_symbol['1']
+    assert_equal :two, @object.string_symbol['two']
+    assert_equal 1.0, @object.integer_float[1]
+    assert_equal 2.2, @object.integer_float[2]
+    assert_equal Set.new([1]), @object.time_set[Time.at(@now.to_i)]
+    assert_equal Set.new([2]), @object.time_set[Time.at(@later.to_i)]
+    assert_equal '1', @object.string_person.keys.first
+    assert_equal 1, @object.person_int.values.first
+    assert Person === @object.string_person.values.first
+    assert Person === @object.person_int.keys.first
+    assert_equal Hash.new, @object.nil
+  end
+
+  def test_modifying_hash
+    @object.string_symbol[3] = 'three'
+    assert_nil @object.string_symbol[3]
+    assert_equal :three, @object.string_symbol['3']
   end
 
   def test_error
