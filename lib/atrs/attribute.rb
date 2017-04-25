fc435dc2dd82df3f3d5f22031eba18d1338dd5c9
Eigil Sagafos <eigil@sagafos.no>
2017-04-25 16:29:17 +0200

    Fixed rubocop style warnings in attribute.rb

--- lib/atrs/attribute.rb
+++ lib/atrs/attribute.rb
@@ -25,40 +25,40 @@ module Atrs
   Getter = Struct.new(:name, :priority, :getter, :use_if)
   Setter = Struct.new(:name, :priority, :setter, :use_if)
 
-  def self.register_setter(name:, priority:, setter:, use_if: ->(atr) {true})
+  def self.register_setter(name:, priority:, setter:, use_if: ->(_atr) {true})
     raise 'Name taken' if setters[name]
-    if res = setters.find { |k,v| v.priority == priority }
+    if (res = setters.find { |_k,v| v.priority == priority })
       raise "Prioriy #{priority} in use by #{res.name}"
     end
     setters[name] = SetterGetter.new(name, priority, setter, use_if)
   end
 
-  def self.register_getter(name:, priority:, getter:, use_if: ->(atr) {true})
+  def self.register_getter(name:, priority:, getter:, use_if: ->(_atr) {true})
     raise 'Name taken' if getters[name]
-    raise 'Prioriy in use' if getters.any? { |k,v| v.priority == priority }
+    raise 'Prioriy in use' if getters.any? { |_,v| v.priority == priority }
     getters[name] = SetterGetter.new(name, priority, getter, use_if)
   end
 
   Atrs.register_getter(
     name: :getter,
     priority: 0,
-    use_if: ->(atr) { true },
-    getter: ->(value, atr, instance) {
+    use_if: ->(_atr) { true },
+    getter: ->(_value, atr, instance) {
       instance.instance_variable_get(atr.ivar_name)
     }
   )
   Atrs.register_setter(
     name: :coercer,
     priority: -100,
-    use_if: ->(atr) { true },
-    setter: ->(value, atr, instance) {
+    use_if: ->(_atr) { true },
+    setter: ->(value, atr, _instance) {
       atr.coerce(value)
     }
   )
   Atrs.register_setter(
     name: :setter,
     priority: 100,
-    use_if: ->(atr) { true },
+    use_if: ->(_atr) { true },
     setter: ->(value, atr, instance) {
       instance.instance_variable_set(atr.ivar_name, value)
     }
@@ -85,8 +85,6 @@ module Atrs
       @options = validate_options(options, defining_object, config, store)
       @getters ||= build_getter_array(defining_object)
       @setters ||= build_setter_array(defining_object)
-      # getters(defining_object)
-      # setters(defining_object)
       freeze
     end
 
@@ -97,14 +95,6 @@ module Atrs
       end
       klass
     end
-
-    # def getters(instance)
-    #   @getters ||= build_getter_array(instance)
-    # end
-    #
-    # def setters(instance)
-    #   @setters ||= build_setter_array(instance)
-    # end
 
     def coerce(val)
       klass.coerce(val, self)
@@ -150,7 +140,7 @@ module Atrs
       end + DEFAULT_SETTERS).compact.sort_by(&:priority)
     end
 
-    def validate_options(options, defining_object, config, store)
+    def validate_options(options, defining_object, config, _store)
       options.each do |name, _|
         raise UnavailableOptionError unless config.option_store[name]
         define_singleton_method(name) { @options[name] }
