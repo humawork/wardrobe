ea0068bb5ede28d268067f557fef42bfcc061c38
Eigil Sagafos <eigil@sagafos.no>
2017-05-08 11:37:52 +0200

    Cleaned up nil if zero plugin

--- lib/atrs/plugins/nil_if_zero.rb
+++ lib/atrs/plugins/nil_if_zero.rb
@@ -1,27 +1,18 @@
 # frozen_string_literal: true
 
 module Atrs
+  register_setter(
+    name: :nil_if_zero,
+    priority: 10,
+    use_if: ->(atr) { atr.options[:nil_if_zero] },
+    setter: lambda do |value, _atr, _instance|
+      value == 0 ? nil : value
+    end
+  )
   module Plugins
     module NilIfZero
       extend Atrs::Plugin
-
-      Atrs.register_setter(
-        name: :nil_if_zero,
-        priority: 10,
-        use_if: ->(atr) { atr.options[:nil_if_zero] },
-        setter: lambda do |value, _atr, _instance|
-          value == 0 ? nil : value
-        end
-      )
-
       option :nil_if_zero, Boolean, setter: :nil_if_zero
-      # setter(10) { |value, atr, instance|
-      #   if value == 0 && atr.nil_if_zero == true
-      #     nil
-      #   else
-      #     value
-      #   end
-      # }
     end
   end
   register_plugin(:nil_if_zero, Plugins::NilIfZero)
