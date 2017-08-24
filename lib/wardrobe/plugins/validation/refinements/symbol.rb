cbe314cf65a5fde393fd2f978a3f7479532d7d63
Eigil Sagafos <eigil@sagafos.no>
2017-08-24 16:00:32 +0200

    Cleanup

--- lib/wardrobe/plugins/validation/refinements/symbol.rb
+++ lib/wardrobe/plugins/validation/refinements/symbol.rb
@@ -11,16 +11,6 @@ module Wardrobe
             'must be empty'
           end
 
-          # def size?(predicate_value)
-          #   puts "asdfasdf"
-          #   binding.pry
-          # end
-          #
-          # def min_size?(predicate_value)
-          #   puts "asdfasdf"
-          #   binding.pry
-          # end
-
           def filled?
             return unless self.to_s.strip == ''
             'must be filled'
