8d4514f6dfdaef33027317808334f0ff8460c9e2
Eigil Sagafos <eigil@sagafos.no>
2017-09-04 20:20:46 +0200

    Removed comment

--- lib/wardrobe/plugins/reset.rb
+++ lib/wardrobe/plugins/reset.rb
@@ -3,7 +3,6 @@ module Wardrobe
     module Reset
       extend Wardrobe::Plugin
 
-
       module InstanceMethods
         def _reset
           initialize
@@ -12,21 +11,6 @@ module Wardrobe
         def _reset!
           self.class.new
         end
-
-        # using MergeRefinements
-        # def merge(other, deep: false)
-        #   raise TypeError, "Class missmatch #{other.class} != #{self.class}" unless other.class == self.class
-        #   merge_type = deep ? :merge : :deep_merge
-        #   res = _attribute_store.inject({}) do |hash, atr|
-        #     hash[atr.name] = send(atr.name).send(merge_type, other.send(atr.name))
-        #     hash
-        #   end
-        #   self.class.new(res)
-        # end
-        #
-        # def deep_merge(other)
-        #   merge(other, deep: true)
-        # end
       end
     end
   end
